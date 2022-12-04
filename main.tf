c7b22009dca20c7f69253a58cdf61e559b6f8847
Janardhan Anipireddy <72906105+areddy548@users.noreply.github.com>
2022-12-04 18:23:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,30 @@
+
+
+
+data "template_file" "userdata" {
+  template = file("${path.module}/hosts.tpl")
+  vars = {
+    key_status = ${var.vmPassword}
+  }
+}
+
+resource "local_file" "foo" {
+    content  = data.template_file.userdata.rendered
+    filename = "${path.module}/hosts"
+}
+
 resource "null_resource" "null01" {
      triggers = {
     always_run = "${timestamp()}"
      }
     provisioner "local-exec" {
     environment = {
-      key_status = ${var.vmPassword}
+      key_status = 
     }
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${path.module}/hosts ${path.module}/ansible-data/playbooks/install-tree.yml"
   }
 }
+
 
 
 variable "vmPassword" {

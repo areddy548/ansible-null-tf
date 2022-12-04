24f699ce84ace31173137a73c153a185e6f74dd4
Janardhan Anipireddy <72906105+areddy548@users.noreply.github.com>
2022-12-04 18:02:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ resource "null_resource" "null01" {
     always_run = "${timestamp()}"
      }
     provisioner "local-exec" {
+    environment = {
+      key_status = ${var.vmPassword}
+    }
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${path.module}/hosts ${path.module}/ansible-data/playbooks/install-tree.yml"
   }
 }
